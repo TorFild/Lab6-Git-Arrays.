6bbf837282720025f2b3dffae7f45304fa5c0474
TorFild <165796925+TorFild@users.noreply.github.com>
2025-12-15 20:33:08 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,19 @@
 #include <windows.h>
 #include <ctime>
 #include <cstdlib>
+
 using namespace std;
 
 int main()
 {
+  
 
     SetConsoleOutputCP(65001);
     SetConsoleCP(65001);
+
+    
+    // 1. Введення та ініціалізація даних
+
     const int SIZE = 14;
     double A[SIZE];
 
@@ -25,6 +31,9 @@ int main()
         cout << A[i] << (i < SIZE - 1 ? ", " : "");
     }
     cout << " ]" << endl;
+
+    
+    // 2. Аналітична обробка масиву
 
     double sumNeg = 0, sumPos = 0;
     int countNeg = 0, countPos = 0;
@@ -46,38 +55,43 @@ int main()
     double mean_negative = (countNeg > 0) ? sumNeg / countNeg : 1;
     double mean_positive = (countPos > 0) ? sumPos / countPos : 0;
 
-   bool allNeg = true, allPos = true;
+    
+    // 3. Логічна обробка масиву
 
-for (int i = 0; i < SIZE; i++)
-{
-    if (A[i] >= 0)
-        allNeg = false;
-    if (A[i] <= 0)
-        allPos = false;
-}
+    bool allNeg = true, allPos = true;
 
-if (allNeg)
-{
-    for (int i = 0; i < SIZE; i++)
-        A[i] = 0;
-}
-else if (allPos)
-{
-    for (int i = 0; i < SIZE; i++)
-        A[i] = 1;
-}
-else
-{
     for (int i = 0; i < SIZE; i++)
     {
-        if (i % 2 == 0 && A[i] < 0)
-            A[i] = mean_negative;
-        else if (i % 2 == 1)
-            A[i] = mean_positive;
+        if (A[i] >= 0)
+            allNeg = false;
+        if (A[i] <= 0)
+            allPos = false;
     }
-}
 
+    if (allNeg)
+    {
+        for (int i = 0; i < SIZE; i++)
+            A[i] = 0;
+    }
+    else if (allPos)
+    {
+        for (int i = 0; i < SIZE; i++)
+            A[i] = 1;
+    }
+    else
+    {
+        for (int i = 0; i < SIZE; i++)
+        {
+            if (i % 2 == 0 && A[i] < 0)
+                A[i] = mean_negative;
+            else if (i % 2 == 1)
+                A[i] = mean_positive;
+        }
+    }
 
+    
+    // 4. Виведення результатів та перевірка
+    
     cout << "Перетворений масив: [ ";
     for (int i = 0; i < SIZE; i++)
     {
